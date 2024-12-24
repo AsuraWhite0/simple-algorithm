94f04fc4678350c2709187e8b7b6e90fe18c07dc
Asura ! <157927637+AsuraWhite0@users.noreply.github.com>
2024-12-24 09:57:00 +0700

    algorithm.py

---
+++ algorithm.py
@@ -0,0 +1,25 @@
+# Those are 2 algorithms of the algorithms i am using the bot.
+    def PastGame(self, safe_amount, mine_positions):
+        if not mine_positions:
+            return "None"
+        if mine_positions:
+            safespot = [mine for game in mine_positions for mine in game][:safe_amount]
+            board = self.board(safespot=safespot)
+        return board,safespot
+
+
+     def Probability(self, safe_amount, mine_positions):
+        if not mine_positions:
+            return "None"
+        mines = np.zeros(25)
+        for locations in mine_positions:
+            for mine in locations:
+                mines[mine] += 1
+        probabilities = mines / len(mine_positions)
+        safespot = np.argsort(probabilities)[:safe_amount]
+        board = self.board(safespot=safespot)
+        return board
+
+#Why use the past mineLocation?
+#> past mineLocation is the location of the bombspot from a old game. Clicking on the past mineLocation is safer than other spots. With the modified algorithms (some using machine learning models), we can increase the accuracy of winning.
+#> Some are use uncoverLocation which is a win-spot to train and find the unsafe or can be bombspot for the next game
